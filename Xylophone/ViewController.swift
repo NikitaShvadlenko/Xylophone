23dca89588b45a6fd73768353333b18bcbb0c3ef
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-10-10 19:16:17 +1300

    Added playMusic functionality to ViewController

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,11 +2,19 @@ import UIKit
 
 class ViewController: UIViewController {
     let mainScreenView = MainScreenView()
+    let player = MusicPlayer()
     override func loadView() {
         view = mainScreenView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainScreenView.setDelegateForButtons(delegate: self)
+    }
+}
+
+extension ViewController: KeyViewDelegate {
+    func keyView(_ keyView: KeyView, didPressButtonFor sound: Sound) {
+        player.playSound(sound: sound)
     }
 }
